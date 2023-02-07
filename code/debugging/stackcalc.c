89ee748c89c3530ece8ebf01a31844ea93063993
Thomas Parr <lw22736@bristol.ac.uk>
2023-02-07 10:05:38 +0000

    Corrected bugs in debugging exercise

--- code/debugging/stackcalc.c
+++ code/debugging/stackcalc.c
@@ -16,16 +16,16 @@ Returns 1 if successful and -1 on error (stack full).
 */
 int push(double d) {
     if (height >= 10) {
-    	return -1;
+    	return 0;
     }	
     stack[height] = d;
     height++;
     return 1;
 }
 
-/* Check if there is something on the stack. */
+/* Check if there are at least 2 values on the stack. */
 int peek() {
-	return height > 0;
+	return height > 1;
 }
 
 /*
@@ -33,8 +33,8 @@ Pop a value off the stack.
 The caller must check that the stack is not empty first.
 */
 double pop() {
-    double d = stack[height];
     height--;
+    double d = stack[height];
     return d;
 }
 
@@ -42,10 +42,10 @@ void calc(const char *line) {
 	double d = 0.0;
 	int is_num = 0; /* nonzero if we're currently parsing a number */
 	int ok;
-	for (const char *p = line; *p != '\0'; *p++) {
+	for (const char *p = line; *p != '\0'; p++) {
 	    /* if we're in a number, then add the digit */
 		if (is_num) {
-			if (*p > '0' && *p <= '9') {
+			if (*p >= '0' && *p <= '9') {
 				d *= 10;
 				d += (double)(*p - '0');
 			} else {
@@ -54,6 +54,7 @@ void calc(const char *line) {
 					printf("Error, stack full.\n");
 					return;
 				}
+				d = 0;
 				is_num = 0;
 			}
 		}
@@ -63,25 +64,27 @@ void calc(const char *line) {
            the previous and the following if clause. */
 		if (!is_num) {
 		    /* if we're not in a number but we see one, then start one */
-			if (*p > '0' && *p <= '9') {
+			if (*p >= '0' && *p <= '9') {
 				d += (double)(*p - '0');
+				is_num = 1;
             /* skip spaces */
 			} else if (*p == ' ') {
 				/* skip */
 			/* deal with operators */
-			} else if (*p == '+' && *p == '-' && *p == '*' && *p == '/') {
+			} else if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
+				// Checks if at least 2 values on stack.
 				ok = peek();
 				if (!ok) { 
-				    printf("Error, operator on empty stack.\n");
+				    printf("Error, operator on too few numbers.\n");
 				    return;
 				}
 				double a = pop();
 				double b = pop();
 				switch (*p) {
-					case '+': a = a + b; break;
-					case '-': a = a - b; break;
-					case '*': a = a * b; break;
-					case '/': a = a / b; break;
+					case '+': a = b + a; break;
+					case '-': a = b - a; break;
+					case '*': a = b * a; break;
+					case '/': a = b / a; break;
 				}
 				push(a); /* can't overflow */
 			} else {
@@ -90,6 +93,13 @@ void calc(const char *line) {
 			}
 		}
 	}
+
+	// Pushes final number onto stack
+	if (d) ok = push(d);
+	if (!ok) {
+		printf("Error, stack full.\n");
+	}
+
 	if (height != 1) {
 		printf("Error, expected exactly one value but got %i.\n", height);
 	} else {
